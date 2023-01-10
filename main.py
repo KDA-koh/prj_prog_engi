57e4488931c7fca0ba3d89c8124f826756c6339a
Drimkore <art.2000m.ab@gmail.com>
2023-01-10 15:58:10 +0500

    working with flask add template to html

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 from transformers import pipeline
 from pydantic import BaseModel
 import uvicorn
+from flask import Flask,render_template,request
+
 
 class Item(BaseModel):
     text: str
 
-app = FastAPI()
-classifier = pipeline("text2text-generation",
-                      model="czearing/story-to-title")
+#app = FastAPI()
+web_app = Flask(__name__)
+#classifier = pipeline("text2text-generation", model="czearing/story-to-title")
 
+@web_app.route("/")
+def hello_world():
+    return render_template('main_page.html')
 
-@app.get("/")
-def root():
-    return {"message": "texttext"}
-
-
-@app.post("/title/")
+@web_app.route('/', methods=['POST'])
+#def my_form_post():
+#    text = request.form['text']
+#    processed_text = text.upper()
+#    return render_template('main_page.html', text = processed_text)
 def predict(item: Item):
     #output = classifier(item.text)
     #return {"I suggest for your text this title": output}
-    return classifier(item.text)
+    return '<h1>hi, my name is...</h1>'
+
+
+
+@web_app.route("/", methods=['POST'])
+def predict(item: Item):
+    #output = classifier(item.text)
+    #return {"I suggest for your text this title": output}
+    return 'hi, my name is...'
+
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='localhost')
+    web_app.run()
