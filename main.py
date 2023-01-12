167f459a6b34c8aa5ace9d117cc2adbae2a93730
Drimkore <art.2000m.ab@gmail.com>
2023-01-12 14:59:25 +0500

    test CI/CD

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from transformers import pipeline
 from flask import Flask,render_template,request
 
-web_app = Flask(__name__)
+app = Flask(__name__)
 classifier = pipeline("text2text-generation", model="czearing/story-to-title")
 
-@web_app.route("/")
+@app.route("/")
 def hello_world():
     return render_template('main_page.html')
 
-@web_app.route('/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     processed_text = classifier(text)[0]
     return render_template('main_page.html', text = processed_text.get('generated_text'))
 
 if __name__ == '__main__':
-    web_app.run()
+    app.run()
