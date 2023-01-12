ebb4d3c98bbd3543c99d88c9f375811e69a45236
Drimkore <art.2000m.ab@gmail.com>
2023-01-12 16:26:50 +0500

    fix

--- test_main.py
+++ test_main.py
@@ -24,4 +24,4 @@ def test_request_example(client):
 def test_predict_dog(client):
     response = client.post("/", json={"text": "A young orphan girl adopts a dog, completely unaware that its supposedly a dangerous scientific experiment thats taken refuge on Earth and is now hiding from its creator"})
     res = response.get_json()
-    assert res['generated_text'] == 'Dog'
+    assert res == {'generated_text': 'Dog'}
