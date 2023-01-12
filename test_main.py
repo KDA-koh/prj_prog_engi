f04b11b0faff3d2bc8353fe4070e39698c9bb728
Drimkore <art.2000m.ab@gmail.com>
2023-01-12 15:35:20 +0500

    test two

--- test_main.py
+++ test_main.py
@@ -1,6 +1,5 @@
 import pytest
 from main import app
-from fastapi.testclient import TestClient
 
 
 @pytest.fixture()
@@ -13,16 +12,17 @@ def test_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'message': "texttext"}
-
-
-def test_predict_dog():
-    response = client.post("/title/", json={"text": "A young orphan girl adopts a dog, completely unaware that its supposedly a dangerous scientific experiment thats taken refuge on Earth and is now hiding from its creator"})
-    if response.status_code == 200:
-        json_data = response.json()
-        assert json_data[0] == {'generated_text': 'Dog'}
 """
+
+
+
 
 def test_request_example(client):
     response = client.get("/")
     assert response.status_code == 200
-    #assert response.json() == {'message': "texttext"}
+
+def test_predict_dog(client):
+    response = client.post("/", json={"text": "A young orphan girl adopts a dog, completely unaware that its supposedly a dangerous scientific experiment thats taken refuge on Earth and is now hiding from its creator"})
+    if response.status_code == 200:
+        json_data = response.json()
+        assert json_data[0] == {'generated_text': 'Dog'}
